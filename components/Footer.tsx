a4ebc009f64cb136589b44e7d6ceb2db66e067d8
Amani Kaneza Fred <95358548+amankanz@users.noreply.github.com>
2025-02-23 13:04:10 +0300

    add: Portfolio link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <p className="mt-6 text-sm text-center">
       &copy; {new Date().getFullYear()} CipherClock by{" "}
       <a
-        href="https://github.com/amankanz"
+        href="https://kanezaio.netlify.app/"
         target="_blank"
         rel="noopener noreferrer"
         className="inline-block text-sm underline hover:no-underline hover:text-gray-900 transition-colors duration-200"
